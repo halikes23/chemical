1ed429de751cd245581012e529aba868a73b9672
Zhen Cheng <chengzhen000000@163.com>
2017-11-09 13:32:30 +0800

    mvn.install.sh add redis redis-interface message

--- mvn.install.sh
+++ mvn.install.sh
@@ -1,21 +1,27 @@
 PROJECT="$1"
 
-if  [ "$PROJECT" != "" ] && [ "$PROJECT" != "qiniu-resource" ] ;
+if  [ "$PROJECT" != "" ] && [ "$PROJECT" != "qiniu-resource" -a "$PROJECT" != "redis" -a "$PROJECT" != "redis-interface"  -a "$PROJECT" != "message" ] ;
 then
 	echo "error parameters : $PROJECT" 
 	exit ;
 fi
 
-cd chemical-qiniu-resource
 
-#cd jnwtv-common
-#mvn clean install -Dmaven.test.skip=true
+cd chemical-common
+mvn clean install -Dmaven.test.skip=true
 
 if [ "$PROJECT" != "" ] ; then
 	cd ../chemical-$PROJECT
 	mvn clean install -Dmaven.test.skip=true
+
 else	
 	cd ../chemical-qiniu-resource;
+	mvn clean install -Dmaven.test.skip=true
+	cd ../chemical-redis;
+	mvn clean install -Dmaven.test.skip=true
+	cd ../chemical-redis-interface;
+	mvn clean install -Dmaven.test.skip=true
+	cd ../chemical-message;
 	mvn clean install -Dmaven.test.skip=true
 fi
 
